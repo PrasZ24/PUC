720279654050c4a64ce6c3180cbbfd6000cf44e4
Prasetya Garda K <157672092+PrasZ24@users.noreply.github.com>
2025-05-30 21:57:03 +0700

    Add files via upload

--- example/GetallData/GetallData.ino
+++ example/GetallData/GetallData.ino
@@ -1,49 +1,45 @@
-#include <puc.h>
+#include <puc_v2.h>
 
 puc myServer;
 
-void setup() {
+void setup()
+{
     Serial.begin(115200);
-    myServer.begin("ESP32_WebServer", "123456789", IPAddress(192, 168, 4, 1), false);
+    myServer.begin("ESP32_WebServer", "123456789", IPAddress(192, 168, 4, 1));
 }
 
-void loop() {
-    int dataLx = myServer.getLX();
-    int dataLy = myServer.getLY();
-    int dataRx = myServer.getRX();
-    int dataRy = myServer.getRY();
-    int buttonA = myServer.getButtonA();
-    int buttonB = myServer.getButtonB();
-    int button1 = myServer.getButton1();
-    int button2 = myServer.getButton2();
-    int button3 = myServer.getButton3();
-    int button4 = myServer.getButton4();
-    int button5 = myServer.getButton5();
-    int button6 = myServer.getButton6();
+void loop()
+{
+    // Important function for captive to run
+    myServer.DNS_RUN();
 
-    Serial.print("Analog Left X  : ");
-    Serial.print(dataLx);
-    Serial.print(" || Analog Left Y  : ");
-    Serial.print(dataLy);
-    Serial.print(" || Analog Right X : ");
-    Serial.print(dataRx);
-    Serial.print(" || Analog Right Y : ");
-    Serial.print(dataRy);
+    if (myServer.device_status()) {
+        int dataLx = myServer.getLX();
+        int dataLy = myServer.getLY();
+        int dataRx = myServer.getRX();
+        int dataRy = myServer.getRY();
+        int buttonA = myServer.getButtonA();
+        int buttonB = myServer.getButtonB();
+        int buttonC = myServer.getButtonC();
+        int buttonD = myServer.getButtonD();
 
-    Serial.print(" || Button A : ");
-    Serial.print(buttonA);
-    Serial.print(" || Button B : ");
-    Serial.print(buttonB);
-    Serial.print(" || Button 1 : ");
-    Serial.print(button1);
-    Serial.print(" || Button 2 : ");
-    Serial.print(button2);
-    Serial.print(" || Button 3 : ");
-    Serial.print(button3);
-    Serial.print(" || Button 4 : ");
-    Serial.print(button4);
-    Serial.print(" || Button 5 : ");
-    Serial.print(button5);
-    Serial.print(" || Button 6 : ");
-    Serial.println(button6);
+        Serial.print("Analog Left X  : ");
+        Serial.print(dataLx);
+        Serial.print(" || Analog Left Y  : ");
+        Serial.print(dataLy);
+        Serial.print(" || Analog Right X : ");
+        Serial.print(dataRx);
+        Serial.print(" || Analog Right Y : ");
+        Serial.print(dataRy);
+        Serial.print(" || Button A : ");
+        Serial.print(buttonA);
+        Serial.print(" || Button B : ");
+        Serial.print(buttonB);
+        Serial.print(" || Button C : ");
+        Serial.print(buttonC);
+        Serial.print(" || Button D : ");
+        Serial.println(buttonD);
+    } else {
+        Serial.println("There isn't any devices connected!");
+    }
 }
